dd8da25c93ec4294aa9cf648fcd27e6ffb98cafb
Achala Eshan <achalaeshan20@gmail.com>
2026-06-10 11:29:13 +0530

    http request metods are crestedd

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-//4ef17996
-function search(){
+function search() {
     console.log("searching...");
 
     let movieName = document.getElementById("movie").value;
     console.log(movieName);
+
+    
+    let url = "https://www.omdbapi.com/?t=" + movieName + "&apikey=4ef17996";
+    console.log(url);
+
+    let http = new XMLHttpRequest();
+    http.open("GET", url);
+    http.responseType = "json";
+    http.send();
+
+    http.onload = function() {
+        let data = http.response;
+        console.log(data);
+
+    };
 }
